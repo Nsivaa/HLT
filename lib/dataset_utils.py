f53db60f034756b3ef4febedaf18e18ffd1ccf6d
Andrea Alberti <a.alberti14@studenti.unipi.it>
2024-04-13 20:20:36 +0200

    data loader, initial roberta code

--- lib/dataset_utils.py
+++ lib/dataset_utils.py
@@ -1,9 +1,12 @@
 
 import pandas as pd
 from enum import Enum, auto
+from torch.utils.data import Dataset, DataLoader
+import torch
+from sklearn.preprocessing import MultiLabelBinarizer
 
 # enum of datasets
-class Dataset(Enum):
+class DatasetEnum(Enum):
     GoEmotions = auto()
     TwitterData = auto()
 
@@ -38,9 +41,67 @@ def load_twitter_data():
     return goemotions_train, goemotions_val, goemotions_test
 
 DATA_LOADERS = {
-    Dataset.GoEmotions: load_goemotions,
-    Dataset.TwitterData: load_twitter_data
+    DatasetEnum.GoEmotions: load_goemotions,
+    DatasetEnum.TwitterData: load_twitter_data
 }
 
-def load_dataset(dataset: Dataset):
+def load_dataset(dataset: DatasetEnum):
     return DATA_LOADERS[dataset]()
+
+class EmotionsData(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.text = dataframe['text']
+        label_series = dataframe['emotions'].apply(lambda x: x.split(','))
+        mlb = MultiLabelBinarizer()
+        self.targets = mlb.fit_transform(label_series)
+        self.label_order = mlb.classes_
+        self.max_len = max_len
+        self.nclasses = len(self.label_order)
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, index):
+        # normalize whitespace
+        text = str(self.text[index])
+        text = " ".join(text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
+    
+def create_data_loader_from_dataframe(dataframe, tokenizer, max_len, **loader_params):
+    ds = EmotionsData(
+        dataframe=dataframe,
+        tokenizer=tokenizer,
+        max_len=max_len
+    )
+
+    return DataLoader(
+        ds,
+        **loader_params
+    )
+
+def create_data_loader(dataset: DatasetEnum, tokenizer, max_len, **loader_params):
+    train, val, test = load_dataset(dataset)
+    train_loader = create_data_loader_from_dataframe(train, tokenizer, max_len, **loader_params)
+    val_loader = create_data_loader_from_dataframe(val, tokenizer, max_len, **loader_params)
+    test_loader = create_data_loader_from_dataframe(test, tokenizer, max_len, **loader_params)
+    return train_loader, val_loader, test_loader
