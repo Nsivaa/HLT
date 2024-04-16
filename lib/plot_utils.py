47ed1575494f3aee77142a8373c495b9fb7a2e4d
Andrea Alberti <a.alberti14@studenti.unipi.it>
2024-04-16 17:38:03 +0200

    threshold tuning generalized

--- lib/plot_utils.py
+++ lib/plot_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 # create confusion matrix of multilabel classification
 def multilabel_confusion_matrix(y_true, y_pred, label_true, label_pred, normalize=False):
@@ -22,3 +23,23 @@ def plot_multilabel_confusion_heatmap(y_true, y_pred, label_true, label_pred, no
     ax.set_xlabel('Predicted')
     ax.set_ylabel('True')
     return ax
+
+def tune_sigmoid_threshold(y_true, y_pred, metric_fun=accuracy_score, metric_params={}, is_maximization=True):
+    thresholds = np.arange(0, 1, 0.01)
+    scores = [metric_fun(y_true, y_pred > t, **metric_params) for t in thresholds]
+    best_threshold = thresholds[np.argmax(scores)] if is_maximization else thresholds[np.argmin(scores)]
+    return best_threshold, scores
+
+def plot_threshold_tuning(y_true, y_pred, metric_fun=accuracy_score, metric_params={}, plot=False, is_maximization=True, metric_name='Accuracy'):
+    best_threshold, scores = tune_sigmoid_threshold(y_true, y_pred, metric_fun, metric_params, is_maximization)
+    if plot:
+        plt.plot(np.arange(0, 1, 0.01), scores)
+        plt.xlabel('Threshold')
+        plt.ylabel(metric_name)
+        # get average type if provided
+        if 'average' in metric_params:
+            metric_name += f' ({metric_params["average"]})'
+        plt.title(f'{metric_name} over sigmoid threshold')
+        plt.show()
+        print(f'Best threshold: {best_threshold}')
+        print(f'Best {metric_name}: {max(scores) if is_maximization else min(scores)}')
