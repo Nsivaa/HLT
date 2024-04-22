7b940963204ec01e909ec664039790b1458a1ca1
Andrea Alberti <a.alberti14@studenti.unipi.it>
2024-04-22 02:03:02 +0200

    separato codice dei modelli dai notebook

---
+++ lib/models.py
@@ -0,0 +1,204 @@
+
+import numpy as np
+import torch
+from tqdm import tqdm
+from transformers import RobertaModel, RobertaTokenizer
+from torch import cuda
+from lib.dataset_utils import *
+from lib.plot_utils import *
+from sklearn.metrics import accuracy_score, jaccard_score, f1_score
+import matplotlib.pyplot as plt
+
+
+'''
+pytorch Roberta model class
+'''
+#TODO cross validation su topologia
+class RobertaClass(torch.nn.Module):
+    def __init__(self, n_classes, frozen_layers=-1):
+        super(RobertaClass, self).__init__()
+        self.l1 = RobertaModel.from_pretrained("roberta-base")
+        if frozen_layers != -1:
+            for param in self.l1.embeddings.parameters():
+                param.requires_grad = False
+            for i in range(frozen_layers):
+                for param in self.l1.encoder.layer[i].parameters():
+                    param.requires_grad = False
+        self.pre_classifier = torch.nn.Linear(768, 768)#TODO add_module?
+        self.dropout = torch.nn.Dropout(0.3)
+        self.classifier = torch.nn.Linear(768, n_classes)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        hidden_state = output_1[0]#TODO ???
+        pooler = hidden_state[:, 0]
+        pooler = self.pre_classifier(pooler)
+        pooler = torch.nn.ReLU()(pooler)
+        pooler = self.dropout(pooler)
+        output = self.classifier(pooler)
+        return output
+    
+    def get_out_dim(self):
+        return self.classifier.out_features
+    
+'''
+utility function to create parameters for the model
+'''
+def create_model_params(optimizer=torch.optim.Adam,
+                        tokenizer=RobertaTokenizer.from_pretrained('roberta-base', truncation=True, do_lower_case=True),
+                        tokenizer_max_len=None,
+                        loader_params={'batch_size': 8, 'shuffle': True, 'num_workers': 0},
+                        loss_function=torch.nn.BCEWithLogitsLoss(),
+                        epochs=1,
+                        learning_rate=1e-05,
+                        val_patience=1,
+                        clip_grad_norm=-1):
+    return {
+        'optimizer': optimizer,
+        'tokenizer': tokenizer,
+        'tokenizer_max_len': tokenizer_max_len,
+        'loader_params': loader_params,
+        'loss_function': loss_function,
+        'epochs': epochs,
+        'learning_rate': learning_rate,
+        'val_patience': val_patience,
+        'clip_grad_norm': clip_grad_norm
+    }
+
+'''
+interface of pytorch models useful for cross validation and to automate model construction and evaluation
+'''
+class SimpleModelInterface:
+    def __init__(self, 
+                 model: torch.nn.Module, 
+                 scores={},
+                 model_params_dict=create_model_params()):
+        self.model = model
+        self.params = model_params_dict
+        self.optimizer = model_params_dict['optimizer'](params=model.parameters(), lr=model_params_dict['learning_rate'])
+        self.scores = scores
+        self.train_scores = {name: [] for name in scores.keys()}
+        self.train_loss = []
+        self.val_scores = {name: [] for name in scores.keys()}
+        self.val_loss = []
+        self.device = 'cuda' if cuda.is_available() else 'cpu'
+        model.to(self.device)
+
+    def _train(self, training_loader, validation_loader=None, save_path=None):
+        self.model.train()
+        #TODO usare confusion matrix?
+        cur_patience = self.params['val_patience']
+        best_val_loss = np.inf
+        for _ in range(self.params['epochs']):
+            tr_loss = 0
+            predictions_acc = []
+            targets_acc = []
+            for _,data in tqdm(enumerate(training_loader, 0)):
+                ids = data['ids'].to(self.device, dtype = torch.long)
+                mask = data['mask'].to(self.device, dtype = torch.long)
+                token_type_ids = data['token_type_ids'].to(self.device, dtype = torch.long)
+                targets = data['targets'].to(self.device, dtype = torch.float)
+
+                outputs = self.model(ids, mask, token_type_ids)
+                loss = self.params['loss_function'](outputs, targets)
+                tr_loss += loss.item()
+                # append predictions and targets
+                predictions_acc.extend(torch.sigmoid(outputs).detach().cpu().numpy())
+                targets_acc.extend(targets.detach().cpu().numpy())
+
+                # backward pass
+                self.optimizer.zero_grad()
+                loss.backward()
+                if self.params['clip_grad_norm'] > -0:
+                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.params['clip_grad_norm'])
+                self.optimizer.step()
+
+            # create numpy arrays
+            predictions_acc = np.array(predictions_acc)
+            targets_acc = np.array(targets_acc)
+            # calculate training scores
+            epoch_loss = tr_loss/len(training_loader)
+            self.train_loss.append(epoch_loss)
+            for name, score in self.scores.items():
+                self.train_scores[name].append(score(targets_acc, predictions_acc))
+
+            # calculate validation scores
+            if validation_loader is not None:
+                val_scores = self.evaluate(validation_loader)
+                for name, score in val_scores.items():
+                    self.val_scores[name].append(score)
+                self.val_loss.append(val_scores['loss'])
+                if val_scores['loss'] < best_val_loss:
+                    best_val_loss = val_scores['loss']
+                    cur_patience = self.params['val_patience']
+                    # save model
+                    if save_path is not None:
+                        torch.save(self.model, save_path)
+                else:
+                    cur_patience -= 1
+                if cur_patience == 0:
+                    break
+
+        # restore best model
+        if save_path is not None and validation_loader is not None:
+            self.model = torch.load(save_path)
+            self.model.to(self.device)
+
+    def fit(self, training_df, validation_df=None):
+        training_loader = create_data_loader_from_dataframe(training_df, self.params['tokenizer'], self.params['tokenizer_max_len'], **self.params['loader_params'])
+        validation_loader = None
+        if validation_df is not None:
+            validation_loader = create_data_loader_from_dataframe(validation_df, self.params['tokenizer'], self.params['tokenizer_max_len'], **self.params['loader_params'])
+        self._train(training_loader, validation_loader)
+
+    def _predict(self, data_loader, accumulate_targets=False):
+        self.model.eval()
+        # initialize target and prediction matrices
+        predictions_acc = []
+        permutation_acc = []
+        targets_acc = []
+        with torch.no_grad():
+            for _, data in tqdm(enumerate(data_loader, 0)):
+                ids = data['ids'].to(self.device, dtype = torch.long)
+                mask = data['mask'].to(self.device, dtype = torch.long)
+                token_type_ids = data['token_type_ids'].to(self.device, dtype=torch.long)
+                targets = data['targets'].to(self.device, dtype = torch.float)
+                outputs = self.model(ids, mask, token_type_ids).squeeze()
+                # append predictions and targets
+                predictions_acc.extend(torch.sigmoid(outputs).detach().cpu().numpy())
+                permutation_acc.extend(data['index'].numpy())
+                if accumulate_targets:
+                    targets_acc.extend(targets.detach().cpu().numpy())
+        # reverse permutation of predictions
+        permutation_acc = np.array(permutation_acc)
+        targets_acc = np.array(targets_acc)
+        predictions_acc = np.array(predictions_acc)
+        return predictions_acc[np.argsort(permutation_acc)], targets_acc[np.argsort(permutation_acc)] if accumulate_targets else None
+
+    def predict(self, testing_df):
+        testing_loader = create_data_loader_from_dataframe(testing_df, self.params['tokenizer'], self.params['tokenizer_max_len'], **self.params['loader_params'])
+        predictions, _ =  self._predict(testing_loader)
+        return predictions
+
+    def evaluate(self, testing_df):
+        testing_loader = create_data_loader_from_dataframe(testing_df, self.params['tokenizer'], self.params['tokenizer_max_len'], **self.params['loader_params'])
+        predictions, targets = self._predict(testing_loader, accumulate_targets=True)
+        # calculate scores
+        scores = {name: score(targets, predictions) for name, score in self.scores.items()}
+        return scores
+
+    def get_train_scores(self):
+        return self.train_scores
+    
+    def get_train_loss(self):
+        return self.train_loss
+
+    def get_val_scores(self):
+        return self.val_scores
+    
+    def get_val_loss(self):
+        return self.val_loss
+    
+    def save_model(self, model_path, vocabulary_path):
+        torch.save(self.model, model_path)
+        self.tokenizer.save_vocabulary(vocabulary_path)
--- lib/plot_utils.py
+++ lib/plot_utils.py
@@ -24,14 +24,17 @@ def plot_multilabel_confusion_heatmap(y_true, y_pred, label_true, label_pred, no
     ax.set_ylabel('True')
     return ax
 
-def tune_sigmoid_threshold(y_true, y_pred, metric_fun=accuracy_score, metric_params={}, is_maximization=True):
+def tune_sigmoid_threshold(y_true, y_pred, metric_fun=accuracy_score, metric_params={}, is_maximization=True, return_only_best=True):
     thresholds = np.arange(0, 1, 0.01)
     scores = [metric_fun(y_true, y_pred > t, **metric_params) for t in thresholds]
-    best_threshold = thresholds[np.argmax(scores)] if is_maximization else thresholds[np.argmin(scores)]
+    best_id = np.argmax(scores) if is_maximization else np.argmin(scores)
+    best_threshold = thresholds[best_id]
+    if return_only_best:
+        scores = scores[best_id]
     return best_threshold, scores
 
 def plot_threshold_tuning(y_true, y_pred, metric_fun=accuracy_score, metric_params={}, plot=False, is_maximization=True, metric_name='Accuracy'):
-    best_threshold, scores = tune_sigmoid_threshold(y_true, y_pred, metric_fun, metric_params, is_maximization)
+    best_threshold, scores = tune_sigmoid_threshold(y_true, y_pred, metric_fun, metric_params, is_maximization, return_only_best=False)
     if plot:
         plt.plot(np.arange(0, 1, 0.01), scores)
         plt.xlabel('Threshold')
